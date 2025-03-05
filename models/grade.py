34460340dfe903dadb97d6af6fba90e92c380d45
Lbraham237 <abrahamvogo@gmail.com>
2025-03-05 15:16:59 +0100

    FEAT: Ajouter le controle du seuil de note.

--- models/grade.py
+++ models/grade.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class SchoolGrade(models.Model):
     _name = 'school.grade'
@@ -7,3 +8,9 @@ class SchoolGrade(models.Model):
     student_id = fields.Many2one('school.student', string='Student', required=True)
     course_id = fields.Many2one('school.course', string='Course', required=True)
     grade = fields.Float(string='Grade', required=True)
+
+    @api.constrains('grade')
+    def _check_grade(self):
+        for record in self:
+            if record.grade < 0 or record.grade > 20:
+                raise ValidationError("La note doit être comprise entre 0 et 20.")
